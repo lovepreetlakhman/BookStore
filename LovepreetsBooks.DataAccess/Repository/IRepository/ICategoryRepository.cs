c9a8ad275af4056bf8a95fa5e6989217ed4b4c01
Lovepreet Singh <105748512+lovepreetlakhman@users.noreply.github.com>
2022-11-18 02:45:51 -0500

    Commit Till Repository folder with the interface IRepository folder

---
+++ LovepreetsBooks.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using LovepreetsBooks.DataAccess.Repository.IRepository;
+using LovepreetsBooks.Models;
+using System;
+using LovepreetsBookStore.DataAccess.Data;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LovepreetsBooks.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+    }
+}
+
