d251446e8f55796461f35d73da914e644a4538ab
Lovepreet Singh <105748512+lovepreetlakhman@users.noreply.github.com>
2022-11-18 02:48:05 -0500

    Created the Admin file and editied it with the provided last code

---
+++ LovepreetsBookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,104 @@
+﻿using LovepreetsBooks.DataAccess.Repository.IRepository;
+using LovepreetsBooks.DataAccess.Repository;
+using LovepreetsBookStore.DataAccess;
+using LovepreetsBookStore.Areas;
+using LovepreetsBookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LovepreetsBooks.Models;
+
+namespace LovepreetsBookStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Upsert(int? id)        // action method for upsert
+        {
+            Category category = new Category();     //using pujasBooks.Model
+
+            if (id == null)
+            {
+                //this is for create
+                return View(category);
+            }
+
+            //this is for edit
+            category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+
+        // use HTTP POST to define the post-action method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category)
+        {
+            if (ModelState.IsValid)   // check all variables in the model (e.g. Name Required) to increase security
+            {
+                if (category.Id == 0)
+                {
+                    _unitOfWork.Category.Add(category);
+                    //_unitOfWork.save();
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(category);
+                }
+
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));     // to see all the categories
+            }
+
+            return View(category);
+        }
+
+
+        // API calls here for Delete
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // return NotFound();
+            var allObj = _unitOfWork.Category.GetAll();
+            return Json(new { data = allObj });
+        }
+
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Category.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _unitOfWork.Category.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete Successfully" });
+        }
+        #endregion
+    }
+}
