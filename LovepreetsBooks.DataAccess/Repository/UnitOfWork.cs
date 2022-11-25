73f6625eb99b9aae7a89d825a5903467c5312037
Lovepreet Singh <105748512+lovepreetlakhman@users.noreply.github.com>
2022-11-24 23:17:35 -0500

    reference of covertype in UnitOfWork

--- LovepreetsBooks.DataAccess/Repository/UnitOfWork.cs
+++ LovepreetsBooks.DataAccess/Repository/UnitOfWork.cs
@@ -14,12 +14,15 @@ namespace LovepreetsBooks.DataAccess.Repository.IRepository
         {
             _db = db;
             Category = new CategoryRepository(_db);
+            CoverType = new CoverTypeRepository(_db);
             SP_Call = new SP_Call(_db);
         }
 
         public ICategoryRepository Category { get; private set; }
 
         public ISP_Call SP_Call { get; private set; }
+
+        public ICoverTypeRepository CoverType { get; private set; } 
 
         public void Dispose()
         {
