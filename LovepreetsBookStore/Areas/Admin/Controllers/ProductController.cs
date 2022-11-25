156d1fde59cffb8de47a0ad8dcb3dc8f4c664dae
Lovepreet Singh <105748512+lovepreetlakhman@users.noreply.github.com>
2022-11-24 23:58:53 -0500

    Product Controller made

---
+++ LovepreetsBookStore/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using LovepreetsBooks.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LovepreetsBookStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
