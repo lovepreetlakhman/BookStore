3d1fe96131606a8c9964ddec816e2f58f6d16045
Lovepreet Singh <105748512+lovepreetlakhman@users.noreply.github.com>
2022-11-24 23:15:49 -0500

    db set for covertype

--- LovepreetsBooks.DataAccess/Data/ApplicationDbContext.cs
+++ LovepreetsBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -14,5 +14,6 @@ namespace LovepreetsBookStore.DataAccess.Data
         {
         }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<CoverType> CoverTypes { get; set; }
     }
 }
