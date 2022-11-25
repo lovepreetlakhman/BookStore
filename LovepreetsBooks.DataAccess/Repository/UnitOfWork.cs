86101b23b996c2da6bc0bbf03f0e3fa69bc4d61c
Lovepreet Singh <105748512+lovepreetlakhman@users.noreply.github.com>
2022-11-24 23:51:50 -0500

    In UnitOfWork interface and class added ref of Product

--- LovepreetsBooks.DataAccess/Repository/UnitOfWork.cs
+++ LovepreetsBooks.DataAccess/Repository/UnitOfWork.cs
@@ -15,6 +15,7 @@ namespace LovepreetsBooks.DataAccess.Repository.IRepository
             _db = db;
             Category = new CategoryRepository(_db);
             CoverType = new CoverTypeRepository(_db);
+            Product = new ProductRepository(_db);
             SP_Call = new SP_Call(_db);
         }
 
@@ -22,7 +23,9 @@ namespace LovepreetsBooks.DataAccess.Repository.IRepository
 
         public ISP_Call SP_Call { get; private set; }
 
-        public ICoverTypeRepository CoverType { get; private set; } 
+        public ICoverTypeRepository CoverType { get; private set; }
+
+        public IProductRepository Product { get; private set; }
 
         public void Dispose()
         {
