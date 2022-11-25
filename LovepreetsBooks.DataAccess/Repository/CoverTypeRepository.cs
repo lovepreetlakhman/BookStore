3d8b0f30ec513adce1b1bcf85ea48aedb0657553
Lovepreet Singh <105748512+lovepreetlakhman@users.noreply.github.com>
2022-11-24 23:17:07 -0500

    created covertype repository  interface and class

---
+++ LovepreetsBooks.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,28 @@
+﻿using LovepreetsBooks.DataAccess.Repository.IRepository;
+using LovepreetsBooks.Models;
+using LovepreetsBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LovepreetsBooks.DataAccess.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CoverTypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(CoverType coverType)
+        {
+            var objFromDb = _db.CoverTypes.FirstOrDefault(s => s.Id == coverType.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = coverType.Name;
+            }
+        }
+    }
+}
---
+++ LovepreetsBooks.DataAccess/Repository/IRepository/ICoverTypeRepository.cs
@@ -0,0 +1,12 @@
+﻿using LovepreetsBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LovepreetsBooks.DataAccess.Repository.IRepository
+{
+    public interface ICoverTypeRepository : IRepository<CoverType>
+    {
+        void Update(CoverType coverType);
+    }
+}
